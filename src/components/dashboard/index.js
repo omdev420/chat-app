2a464ba7e7a86aa6a7206b7b8674ec60a2be94b3
omdev420 <omdev420@gmail.com>
2021-07-20 23:10:13 +0530

    started building, added signout button and displayed username

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dasboard = () => {
-  return <div>hello</div>;
+const Dasboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign Out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dasboard;
