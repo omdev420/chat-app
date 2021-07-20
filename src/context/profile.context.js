d7813ac66bf9f6e0ec3cb05a3bc9b4dfbfe321b5
omdev420 <omdev420@gmail.com>
2021-07-21 01:58:25 +0530

    added avatar to the profile context

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -16,12 +16,13 @@ export const ProfileProvider = ({ children }) => {
         userRef = database.ref(`/profiles/${authObj.uid}`);
 
         userRef.on('value', snap => {
-          const { name, createdAt } = snap.val();
+          const { name, createdAt, avatar } = snap.val();
           const data = {
             name,
             createdAt,
             uid: authObj.uid,
             email: authObj.email,
+            avatar,
           };
           setProfile(data);
           setIsLoading(false);
