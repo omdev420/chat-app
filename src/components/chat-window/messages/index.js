d5f03efaa2f43933f98dda089957aa936e22e33e
omdev420 <omdev420@gmail.com>
2021-07-24 17:03:06 +0530

    shown audio msg and deleted files on delete msg

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable consistent-return */
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { database, auth } from '../../../misc/firebase';
+import { storage, database, auth } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -59,7 +60,7 @@ const Messages = () => {
   }, []);
 
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId, file) => {
       // eslint-disable-next-line no-alert
       if (!window.confirm('Delete this message?')) {
         return;
@@ -86,7 +87,15 @@ const Messages = () => {
         await database.ref().update(updates);
         Alert.info('Message has been deleted', 4000);
       } catch (err) {
-        Alert.error(err.message, 4000);
+        return Alert.error(err.message, 4000);
+      }
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message, 4000);
+        }
       }
     },
     [chatId, messages]
